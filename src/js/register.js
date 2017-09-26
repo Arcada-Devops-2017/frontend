31c51798d8c693965fe0d7d7de6ec56ff9b5a5e8
hunghuytran <hungbriantran@gmail.com>
2017-09-26 23:31:19 +0300

    linked javascript register with signin.html

--- src/js/register.js
+++ src/js/register.js
@@ -1,41 +1,44 @@
-/*
-@dev not sure if this is working.
-*/
-function register(){
-  /*
-  @dev set the authentication.
-  */
-  var auth = "auth.arcada.nitor.zone/";
+$(document).ready(function(){
+    $("#register_button").click(function(){
+      /*
+      @dev set the authentication.
+      */
+      var auth = "auth.arcada.nitor.zone/";
 
-  /*
-  @dev get input information from html fields.
-  */
-  var input = {
-    first_name: $("first_name").val(),
-    last_name: $("last_name").val(),
-    username: $("username").val(),
-    password: $("password").val()
-  }
+      /*
+      @dev get input information from html fields.
+      */
+      var input = {
+        first_name: $("firstname").val(),
+        last_name: $("lastname").val(),
+        username: $("email").val(),
+        password: $("psw").val()
+      }
 
-  /*
-  @dev connect to url, convert data to JSON, post data.
-  */
-  $.ajax({
-     url: auth + "register.php",
-     method: "POST",
-     data: JSON.stringify(input),
-     dataType: 'application/json',
-     contentType: "application/json",
-     success: function (result) {
-       /*
-       @dev if success then redirect to homepage or login page for example.
-       */
-       alert(result.status);
-     },
-     error: function(){
-       /*
-       @dev should alert the failure data.
-       */
-     }
-  });
-}
+      /*
+      @dev connect to url, convert data to JSON, post data.
+      */
+      $.ajax({
+         url: auth + "register.php",
+         method: "POST",
+         data: JSON.stringify(input),
+         dataType: 'application/json',
+         contentType: "application/json",
+         success: function (result) {
+           /*
+           @dev if success then redirect to homepage or login page for example.
+           */
+           if(result.status == 200) {
+             alert("Status: " +result.status)
+           } else {
+             alert("Error, failed:" +result.status)
+           }
+        },
+         error: function(){
+           /*
+           @dev should alert the failure data.
+           */
+         }
+      });
+    });
+});
