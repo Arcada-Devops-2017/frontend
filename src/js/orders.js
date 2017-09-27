99e432e1e66ddc3345baa7d8a82b92a722c36c7c
BjornBoman <31731412+BjornBoman@users.noreply.github.com>
2017-09-27 19:30:36 +0300

    Changed code to use the API, removed redundant code

--- src/js/orders.js
+++ src/js/orders.js
@@ -8,9 +8,9 @@ function orders(){
   @dev nuff said. specify the domain which has the api.
   */
 
-  //var token = window.localStorage.getItem(AuthToken);
-  //var url = "orders.arcada.nitor.zone/";
-  var url = 'http://people.arcada.fi/~bomanbjo/DevOps/ordersReturnJson.json';
+  var token = window.localStorage.getItem(AuthToken);
+  var url = 'orders.arcada.nitor.zone/api/FetchAll?AuthToken="'+token+'"';
+  //var url = 'http://people.arcada.fi/~bomanbjo/DevOps/ordersReturnJson.json';
 
   /*
   @dev the function require a token in the parameter.
@@ -18,49 +18,35 @@ function orders(){
        the shipping information is displayed in a html id named shipping_info.
   */
   var jsonResponse;
+  //makes the API-call for orders
   fetch(url).then(function(response) {
 		// Convert to JSON   
-		
 		return response.json();
 	}).then(function(orderResponse) {
 		// orderResponse is a javascript json object
 		console.log(orderResponse); 
 		var jsonResponse = orderResponse.orderData;
-		
-		console.log(orderResponse.orderData[0]);
-		//document.getElementById("dummy").innerHTML = orderResponse.orderData[0].orderDate;
-		
-		//dummyn--->
-		var test = {test:"origin"}; 
-		var ordersBeer1 = {id:0, storeId:"Doe", amount:"46", price:"92"};//price? total or per item
-		var productsBeer1 = {productName:"Corona", price:"2", picture:"media/corona.png"};
-		//<---
-		
-		/*******/
-		/**************/
-		//http://www.9bitstudios.com/2016/01/asynchronous-processes-inside-of-a-loop-in-javascript/
-		/**************/
-		/*******/
-		
+
 		var myTable = ""; 
+		
+		//writes html code for boxes and tables into a 
 		for(i = 0;i < orderResponse.orderData.length; i++){
 				
 			var productId = jsonResponse[i].product.id;
 			var productAmount = jsonResponse[i].product.amount;
-			console.log("TEST 2: "+productAmount);
 				
-			//http://product.arcada.nitor.zone/api/products.php?id=0
-			var url = 'http://people.arcada.fi/~bomanbjo/DevOps/productJsonId=' + productId + '.json';
+			//var url = 'http://people.arcada.fi/~bomanbjo/DevOps/productJsonId=' + productId + '.json';
+			var url = 'http://product.arcada.nitor.zone/api/products.php?id=' + productId;
 				
+				
+			//makes the API-call for products
 			fetch(url).then(function(response2) {
 				// Convert to JSON   
 				return response2.json();
 				
 			}).then(function(productResponse) {
-				console.log(productResponse); 
-				console.log("TEST 2: "+productAmount);
-				//console.log("TEST 3: "+jsonResponse[i].product.amount);
-			
+				
+				//makes html code for floating boxes with tabeles in them 
 				myTable+= '<div class="floating-box"><table><tr><th colspan="2">Product lable to make boxes constant width</td></tr><tr><td rowspan="5"><img src="';
 				myTable+= productResponse.products[0].picture;//product image
 				myTable+= '" alt="';
@@ -77,17 +63,16 @@ function orders(){
 				myTable+= productResponse.products[0].price * productAmount;// totalprice
 				myTable+= '€</td></tr></table></div>';
 				
-				document.getElementById("dummy").innerHTML = myTable;
+				document.getElementById("orders-box").innerHTML = myTable;
 			})
 			
 		}
-		//var myTable = '<p>humbug</p>';
-			
-		
+
 	});
 	
-  //$.getJSON(url /*+ "api/FetchAll?AuthToken='" + token + "'"*/, function(result) {
-    /*$('#order_info').append(result.orderId + ":" + result.orderDate);
+	/*
+  $.getJSON(url + "api/FetchAll?AuthToken='" + token + "'", function(result) {
+    $('#order_info').append(result.orderId + ":" + result.orderDate);
     $.each(result.product, function() {
       $('#order_list').append(
         this.id + " "
