f9966a29633aceaab3da47fd5ae1b18f8fcd2daa
hunghuytran <hungbriantran@gmail.com>
2017-09-28 15:48:24 +0300

    changed added http:// at beginning

--- src/js/stores.js
+++ src/js/stores.js
@@ -7,7 +7,7 @@ function stores(){
   @dev display all existing stores on html id list_stores.
        iterate through the existing list of stores.
   */
-  var url = "stores.arcada.nitor.zone/";
+  var url = "http://stores.arcada.nitor.zone/";
 
   /*
   @dev get json from url, specify the data on which row on .each.
