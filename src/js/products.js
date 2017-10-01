c1627d199937073ae5c05adeda7875d0e68ddd98
hunghuytran <hungbriantran@gmail.com>
2017-10-01 14:43:38 +0300

    changed the code for better approach

--- src/js/products.js
+++ src/js/products.js
@@ -3,6 +3,46 @@
 */
 products();
 
+function productStrings(products, i) {
+  var products = "\
+      <div class='flex-item' onclick='document.getElementById('product"+products[i].id+"').style.display='block'' style='width:auto;'> \
+        <p class='price'>"+products[i].price+"</p> \
+        <div class='align-middle'> \
+            <div class='product-image'>"+products[i].picture+"</div> \
+            <p class='product-name'>"+products[i].name+"</p> \
+            <p class='product-origin'></p> \
+            <p class='product-category'>"+products[i].category+"</p> \
+        </div> \
+        <div id='product"+products[i].id+"' class='modal'> \
+          <div class='modal-content animate'> \
+            <div class='container'> \
+                <div class='preview-image'>"+products[i].picture+"</div> \
+              <div class='information-box'> \
+                  <h1 class='price-preview'>"+products[i].price+"</h1> \
+                  <p class='product-name-preview'>"+products[i].name+"</p> \
+                  <p class='product-category-preview'>"+products[i].category+"</p>\
+                  <p class='product-origin-preview'></p> \
+
+                  <p class='description'>"+products[i].description+"</p> \
+                  <p class='tags'>"+products[i].tags+"</p> \
+                <div class='availability'> \
+                  <h3>Available in</h3> \
+                    <ul> \
+                      <li class='green' onload='checkAvailable();'>hurr hurr - shop</li> \
+                      <li class='red' onload='checkAvailable();'>hurr hurr2 - shop</li> \
+                    </ul> \
+                </div> \
+              </div> \
+                <button type='button' class='button' onclick='addToCart();'>Buy</button> \
+            </div> \
+          </div> \
+        </div> \
+      </div> \
+      ";
+      return products;
+}
+
+
 function products(){
 
   /*
@@ -20,39 +60,8 @@ function products(){
     var products = data.products;
 
     for(var i in products) {
-      $("div[class*='flex-container wrap']")
-      .append("<div class='flex-item' onclick='document.getElementById('product"+products[i].id+"').style.display='block'' style='width:auto;'>")
-      .append("<p class='price'>"+products[i].price+"</p>")
-      .append("<div class='align-middle'>")
-      .append("<div class='product-image'>"+products[i].picture"</div>")
-      .append("<p class='product-name'>"+products[i].name+"</p>")
-      .append("<p class='product-origin'></p>")
-      .append("<p class='product-category'>"+products[i].category"</p>")
-      .append("</div>")
-      .append("<div id='product"+products[i].id+"' class='modal'>")
-      .append("<div class='modal-content animate'>")
-      .append("<div class='container'>")
-      .append("<div class='preview-image'></div>")
-      .append("<div class='information-box'>")
-      .append("<h1 class='price-preview'>"+products[i].price"</h1>")
-      .append("<p class='product-name-preview'>"+products[i].name+"</p>")
-      .append("<p class='product-category-preview'>"+products[i].category"</p>")
-      .append("<p class='product-origin-preview'></p>")
-      .append("<p class='description'>"+products[i].description+"</p>")
-      .append("<p class='tags'>"+products[i].tags+"</p>")
-      .append("<div class='availability'>")
-      .append("<h3>Available in</h3>")
-      .append("<ul>")
-      .append("<li class='green' onload='checkAvailable()'>hurr hurr - shop</li>")
-      .append("<li class='red' onload='checkAvailable()'>hurr hurr - shop</li>")
-      .append("</ul>")
-      .append("</div>")
-      .append("</div>")
-      .append("<button type='button' class='button' onclick='addToCart()'>Buy</button>")
-      .append("</div>")
-      .append("</div>")
-      .append("</div>")
-      .append("</div>");
+
+        $("div[class*='flex-container wrap']").append(productStrings(products, i));
     }
   });
 }
